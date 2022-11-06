7aca5244c059918cb1e6502b5bd6e81fd85e967e
Ivo Serra <ivoozmarserra@gmail.com>
2022-11-06 20:16:38 -0300

    mejorando el carrito

--- js/carrito.js
+++ js/carrito.js
@@ -7,8 +7,8 @@ class Zapatilla {
     this.cuanto = 1;
     }
 }
-const deHombre = new Zapatilla (1,"Fila",15000,"../images/hombres-shop.jpg");
-const deMujer = new Zapatilla (2,"So Sport",17000,"../images/mujer-shop.jpg");
+const deHombre = new Zapatilla (1,"Fila",15.000,"../images/hombres-shop.jpg");
+const deMujer = new Zapatilla (2,"So Sport",17.000,"../images/mujer-shop.jpg");
 
 //Array productos
 
@@ -20,31 +20,33 @@ let carrito = [];
 const almacenamientoProductos = document.getElementById("almacenamientoProductos");
 
 const productosVisibles = () => {
-    productos.forEach((zapatillas) => {
+    productos.forEach((producto) => {
         const card = document.createElement("div");
         card.classList.add("col-xl-3","col-md-6","col-xs-12");
         card.innerHTML = `
             <div class="card">
-                <img src="${zapatillas.imagen}" class="card-img-top" alt="${zapatillas.nombre}">
+                <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
                 <div>
-                <h5 class= "text-center">${zapatillas.nombre}</h5>
-                <p class= "text-center">${zapatillas.precio}</p>
-                <button class="btn btn-primary " id="boton ${zapatillas.id}"> Agregar al carrito </button>
+                <h5 class= "text-center">${producto.nombre}</h5>
+                <p class= "text-center">${producto.precio}</p>
+                <button class="btn btn-primary " id="boton ${producto.id}"> Agregar al carrito </button>
                 <div>
             </div>
         `
         almacenamientoProductos.appendChild(card);
 
-        const productoSumado = document.getElementById(`boton ${zapatillas.id}`)
+        const productoSumado = document.getElementById(`boton ${producto.id}`)
         productoSumado.addEventListener("click", () =>{
             aniadirAlCarrito(productos.id)
         })
+        
     })
 
 }
+
 const aniadirAlCarrito = (id) => {
-    const chequeo = productos.find((productos) => productos.id === id);
-    const chequeoEnCarrito = carrito.find((productos) => productos.id === id);
+    const chequeo = productos.find((producto) => producto.id === id);
+    const chequeoEnCarrito = carrito.find((producto) => producto.id === id);
     if(chequeoEnCarrito){
         chequeoEnCarrito.cuanto++;
     }else{
@@ -55,28 +57,34 @@ productosVisibles ()
 
 const contenedorCarrito = document.getElementById ("contenedorCarrito")
 const verCarrito = document.getElementById ("chequearCarrito")
-verCarrito.addEventListener("click", () => (
+verCarrito.addEventListener("click", () => {
     mostrarCarrito()
-))
+})
 const mostrarCarrito = () => {
     contenedorCarrito.innerHTML="";
-    productos.forEach((zapatillas) => {
+    carrito.forEach((producto) => {
         const card = document.createElement("div");
         card.classList.add("col-xl-3","col-md-6","col-xs-12");
         card.innerHTML = `
             <div class="card">
-                <img src="${zapatillas.imagen}" class="card-img-top" alt="${zapatillas.nombre}">
+                <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
                 <div>
-                <h5 class= "text-center">${zapatillas.nombre}</h5>
-                <p class= "text-center">${zapatillas.precio}</p>
-                <button class="btn btn-primary " id="eliminar${zapatillas.id}"> Borrar producto </button>
+                <h5 class= "text-center">${producto.nombre}</h5>
+                <p class= "text-center">${producto.precio}</p>
+                <p class= "text-center">${producto.cuanto}</p>
+                <button class="btn btn-primary " id="eliminar${producto.id}"> Borrar producto </button>
                 <div>
             </div>
         `
         contenedorCarrito.appendChild(card);
-        const productoRestado = document.getElementById(`eliminar${zapatillas.id}`)
+        const productoRestado = document.getElementById(`eliminar${producto.id}`)
         productoRestado.addEventListener("click", () =>{
-            borrarDelCarrito(productos.id)
+            borrarDelCarrito(producto.id)
+
         })
     })
+}
+
+const borrarDelCarrito = (id) => {
+
 }
